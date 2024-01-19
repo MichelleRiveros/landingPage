fef1f15cc06fd0760e7d70324dd837aa6174c443
MichelleRiveros <michele.riveros2303@hotmail.com>
2024-01-19 07:47:46 -0500

    "Trabajo finalizado"

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let nav = document.querySelector('nav.lista');
-document.querySelector('boton').addEventListener('click',
+document.querySelector('.boton').addEventListener('click',
 function(){
     nav.classList.toggle('active');
 });
