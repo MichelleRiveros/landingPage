8b22869aea94b8480dd3d77f9fd73b56e438b4c1
MichelleRiveros <michelle.riveros2303@hotmail.com>
2024-01-18 21:22:25 -0500

    Profe, no se que hice, solo le cambie el color al containerXD

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-// AOS.init({
-//     offset:1
-// });
+let nav = document.querySelector('nav');
+document.querySelector('boton').addEventListener('click',
+function(){
+    nav.classList.toggle('active');
+});
